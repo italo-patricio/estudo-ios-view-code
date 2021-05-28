6d5ddab0f1e57578a7156cad50771c9dcc3c035a
Italo Patricio <login+italo-patricio@elo7.com>
2021-05-28 10:48:53 -0300

    adicionando snapkit ao projeto

--- EstudoViewCode/CodeView.swift
+++ EstudoViewCode/CodeView.swift
@@ -1,4 +1,5 @@
 import Foundation
+import SnapKit
 
 protocol CodeView {
     func buildViewHierrachy()
--- EstudoViewCode/ViewControllerScreen.swift
+++ EstudoViewCode/ViewControllerScreen.swift
@@ -27,10 +27,17 @@ extension ViewControllerScreen: CodeView {
     }
     
     func setupConstraints() {
-        button.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 15).isActive = true
-        button.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -15).isActive = true
-        button.heightAnchor.constraint(equalToConstant: 50).isActive = true
-        button.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -15).isActive = true
+//        button.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 15).isActive = true
+//        button.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -15).isActive = true
+//        button.heightAnchor.constraint(equalToConstant: 50).isActive = true
+//        button.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -15).isActive = true
+        
+        button.snp.makeConstraints { make in
+            make.left.equalToSuperview().offset(15)
+            make.right.equalToSuperview().inset(15)
+            make.height.equalTo(50)
+            make.bottom.equalTo(self).inset(15)
+        }
     }
     
     func setupAdditionalConfiguration() {
